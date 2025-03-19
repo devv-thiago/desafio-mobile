de3782f52d7f625d3c7454b2d9d5fda0c0f59f1c
devv-thiago <tfofano3@gmail.com>
2025-03-19 20:25:38 -0300

    adicionando rota nomeada e com parametros para detalhes dos filmes

--- lib/ui/homepage/widgets/movie_miniature.dart
+++ lib/ui/homepage/widgets/movie_miniature.dart
@@ -19,7 +19,7 @@ class MovieMiniature extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () => context.goNamed('/movie_detail'),
+      onTap: () => context.goNamed('/movie_detail',extra: movie),
       child: Container(
         decoration: BoxDecoration(),
         width: deviceInfo.width,
--- lib/ui/movie_detail/movie_detail.dart
+++ lib/ui/movie_detail/movie_detail.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/domain/models/movie.dart';
+import 'package:movie_app/ui/core/colors.dart';
 
 class MovieDetail extends StatelessWidget {
   final Movie movie;
-  const MovieDetail({required this.movie, super.key});
+  const MovieDetail({
+    required this.movie,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    Size deviceInfo = MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        Container(
+        height: deviceInfo.height,
+        width: deviceInfo.width,
+        decoration: BoxDecoration(
+          color: AppColors.background,
+          
+        ),
+      ),
+      ], 
+    );
   }
 }
